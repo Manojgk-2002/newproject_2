67eb19a2fb6ba76d1abf00e02b29796ab2a1cbac
Manoj G K <143336735+Manojgk-2002@users.noreply.github.com>
2024-11-27 20:50:36 +0530

    Update security_group.tf

---
+++ security_group.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+}
+resource "aws_instance" "ubuntu"{
+	ami=var.ami
+	instance_type=var.inst_type
+}
+
+
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  tags = {
+    Name = "allow_tls"
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_eip" "elasti_ip"{
+domain   = "vpc"
+instance= aws_instance.ubuntu.id
+}
